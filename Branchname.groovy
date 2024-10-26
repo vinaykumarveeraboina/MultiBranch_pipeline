a9aefa385a051e2fb9a1eb2c99b78de55982adc2
vinaykumarveeraboina <51791892+vinaykumarveeraboina@users.noreply.github.com>
2024-10-26 12:35:51 +0530

    Create Branchname.groovy

---
+++ Branchname.groovy
@@ -0,0 +1,31 @@
+pipeline{
+    agent any
+    stages{
+        
+        stage (deploy to dev){  //we are saying excute this job only in develop (job)
+            when {
+                expression {
+                    BRANCH_NAME ==~ / (develop)  / 
+                }
+              }  
+              steps{
+                echo "deploying to develop branch "
+              }
+          
+            }
+       
+        stage ('deploy to stage or production ') {
+            when {
+                expression {
+                    BRANCH_NAME ==~ / (production|staging)  /     
+                    /*BRANCH_NAME is defualut variable in jenins and only used in multi branch pipeline*/ /* deploy execute this stage only in prodution or staging job ,*/
+                } 
+                }
+             steps{
+                  echo " deploying to '${BRANCH_NAME}'"
+                  
+                }
+        }
+     }
+}
+  
